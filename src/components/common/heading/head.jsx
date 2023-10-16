779a613a71365e7621c9a2a71c19eb5c94541426
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2023-10-16 20:06:33 +0700

    first commit

---
+++ src/components/common/heading/head.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const head = () => {
+  return (
+    <>
+      <section className="head">
+        <div className="container flexSB">
+          <div className="logo">
+            <h1> BAGUS</h1>
+            <span>Lorem ipsum dolor, sit amet Repellat, culpa!</span>
+          </div>
+
+          <div className="social">
+            <i className="fab fa-facebook-f icon"></i>
+            <i className="fab fa-instagram icon"></i>
+            <i className="fab fa-twitter icon"></i>
+            <i className="fab fa-youtube icon"></i>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default head;
